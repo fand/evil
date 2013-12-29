60fd612b8419e77d63a56e2fc623f75977bce2cd
fand <s0811489@coins.tsukuba.ac.jp>
2013-12-29 09:16:20 +0000

    config file

--- script/pro.sh
+++ script/pro.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/sh
+#!/bin/sh
 
 export PLACK_ENV=production
-carton exec perl -Ilib script/wasynth2-server --host=0.0.0.0 -p 9000
+carton exec perl -Ilib script/wasynth2-server --host=0.0.0.0 -p 9000 -c config/production.pl
