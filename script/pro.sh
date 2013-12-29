8de92279b547303d9bc317d9ca94b5c9e9544a6b
fand <s0811489@coins.tsukuba.ac.jp>
2013-12-29 12:39:11 +0000

    production host

--- script/pro.sh
+++ script/pro.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 export PLACK_ENV=production
-carton exec perl -Ilib script/wasynth2-server --host=0.0.0.0 -p 9000 -c config/production.pl
+carton exec perl -Ilib script/wasynth2-server -c config/production.pl
