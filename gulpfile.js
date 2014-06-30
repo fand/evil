7858dfa5ebacdb45914584a47c74183aaf351081
fand <fand>
2014-07-01 03:17:24 +0900

    add gulp-sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var coffee = require('gulp-coffee');
 var plumber = require('gulp-plumber');
 var changed = require('gulp-changed');
 var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
 
 // Server process handler
 var nodemon;
@@ -14,19 +15,32 @@ gulp.task('coffee-update', function(){
   return gulp.src('client/**/*.coffee')
     .pipe(changed('client/**/*.coffee'))
     .pipe(plumber())
+    .pipe(sourcemaps.init())
     .pipe(coffee())
     .pipe(uglify())
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest('client'));
 });
 
 // Compile all coffee file.
-gulp.task('coffee', function () {
+gulp.task('coffee-dev', function () {
+  return gulp.src('client/**/*.coffee')
+    .pipe(plumber())
+    .pipe(sourcemaps.init())
+    .pipe(coffee())
+    .pipe(uglify())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('client'));
+});
+gulp.task('coffee-pro', function () {
   return gulp.src('client/**/*.coffee')
     .pipe(plumber())
     .pipe(coffee())
     .pipe(uglify())
     .pipe(gulp.dest('client'));
 });
+gulp.task('coffee', ['coffee-dev']);
+
 
 // Keep the server alive.
 gulp.task('nodemon', function () {
@@ -62,4 +76,4 @@ gulp.task('serve-pro', function () {
 
 
 // Build all coffee and launch the server.
-gulp.task('default', ['coffee', 'serve-dev']);
+gulp.task('default', ['coffee-dev', 'serve-dev']);
