0c513f858c82d99ae525793607a65223a8b81349
fand <fand@gmork.in>
2015-02-03 23:35:41 +0900

    draw circles for value and range

--- src/client/components/Knob.js
+++ src/client/components/Knob.js
@@ -3,11 +3,17 @@
 
 var React = require('react');
 
+const colorMain = 'rgb(80, 180, 255)';
+const colorSub  = 'rgb(220, 220, 220)';
+
+
+
 const DEGREE = (Math.PI / 180.0);
 const STARTANGLE = -225 * DEGREE;
+const ENDANGLE = 45 * DEGREE;
 const RATIO = (1.0 / 1000.0) * 270.0 * DEGREE;
 
-var getEndAngle = function (value) {
+var getCurrentAngle = function (value) {
   return value * RATIO + STARTANGLE;
 };
 
@@ -25,32 +31,46 @@ var Knob = React.createClass({
     this.data.canvas = this.refs.canvas.getDOMNode();
     this.data.ctx = this.data.canvas.getContext('2d');
 
-    this.data.ctx.lineWidth = this.props.lineWidth || 5;
-    this.data.ctx.strokeStyle = 'rgba(80, 180, 255, 1.0)';
+    this.data.lineWidth = this.props.lineWidth || 7;
 
     this.data.size = this.props.size || 100;
     this.data.mid = this.data.size * 0.5;
-    this.data.radius = this.props.radius || this.data.size * 0.5 - this.data.ctx.lineWidth * 2;
+    this.data.radius = this.props.radius || this.data.size * 0.4 - this.data.lineWidth * 2;
 
     this.render();
   },
   render: function() {
     if (this.data.ctx) {
       this.data.ctx.clearRect(0, 0, this.data.size, this.data.size);
-      this.data.ctx.beginPath();
-      var endAngle = getEndAngle(this.state.value);
 
-      this.data.ctx.arc(this.data.mid, this.data.mid, this.data.radius, STARTANGLE, endAngle, false);
+      var currentAngle = getCurrentAngle(this.state.value);
+      this.data.ctx.lineWidth = this.data.lineWidth;
+
+      this.data.ctx.strokeStyle = colorSub;
+      this.data.ctx.beginPath();
+      this.data.ctx.arc(this.data.mid, this.data.mid, this.data.radius, STARTANGLE, ENDANGLE, false);
       this.data.ctx.stroke();
+
+      this.data.ctx.strokeStyle = colorMain;
+      this.data.ctx.beginPath();
+      this.data.ctx.arc(this.data.mid, this.data.mid, this.data.radius, STARTANGLE, currentAngle, false);
+      this.data.ctx.stroke();
+
     }
 
     return (
       <div>
         name: {this.props.name}
-        value: {this.state.value / 1000.0 * (this.props.max - this.props.min)}
         <canvas key={this.props.name} ref="canvas" width={this.data.size} height={this.data.size}></canvas>
+        <div>
+          <input type="range" value={this.state.value} min="0" max="1000" onChange={this.onChange} />
+        </div>
+        value: {this.state.value / 1000.0 * (this.props.max - this.props.min)}
       </div>
     );
+  },
+  onChange: function (e) {
+    this.setState({value: e.target.value});
   }
 });
 
