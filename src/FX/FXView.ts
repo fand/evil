da1af940e516e05074c25a1bea6789a038ed64ce
AMAGI / Jun Yuri <mail@amagi.dev>
2026-01-03 15:18:15 -0800

    fix: dom fech

--- src/FX/FXView.ts
+++ src/FX/FXView.ts
@@ -3,14 +3,12 @@ import type { FX } from './FX';
 export class FXView {
   model: FX;
   dom: HTMLElement;
-  dom_side: HTMLElement;
   minus_side: HTMLElement;
 
   constructor(model: FX, dom: HTMLElement) {
     this.model = model;
     this.dom = dom;
-    this.dom_side = this.dom.querySelector('.sidebar-effect')!;
-    this.minus_side = this.dom_side.querySelector('.sidebar-effect-minus')!;
+    this.minus_side = this.dom.querySelector('.sidebar-effect-minus')!;
   }
 
   initEvent() {
