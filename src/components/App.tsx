a5e785397912488d5dcddeb84fbd94003bf2b4bf
AMAGI / Jun Yuri <mail@amagi.dev>
2026-01-03 23:07:49 -0800

    feat: create unified App component with all UI

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,62 @@
 /**
  * Root React component.
+ * Unified layout containing all UI components.
  */
+import { SceneParams } from './player/SceneParams';
+import { TransportButtons } from './player/TransportButtons';
+import { NavigationButtons } from './player/NavigationButtons';
+import { useResizeHandler } from './player/useResizeHandler';
+import { SessionGrid } from './session';
+import { InstrumentsContainer } from './instruments';
 import { SaveDialog } from './session/SaveDialog';
+import { SidebarContainer } from './sidebar/SidebarContainer';
 
 export function App() {
+  useResizeHandler();
+
   return (
     <>
+      {/* Main wrapper */}
+      <div id="wrapper">
+        <div id="env"></div>
+
+        {/* Instruments */}
+        <div id="instruments" className="clearfix">
+          <InstrumentsContainer />
+        </div>
+
+        {/* Mixer */}
+        <div id="mixer">
+          <div id="mixer-body">
+            <SessionGrid />
+          </div>
+
+          <div id="mixer-sidebar">
+            <SidebarContainer />
+          </div>
+        </div>
+      </div>
+
+      {/* Footer */}
+      <footer>
+        <SceneParams />
+        <TransportButtons />
+        <div id="song-info">
+          <div>
+            <label>title: </label>
+            <input type="text" id="song-title" />
+          </div>
+          <div>
+            <label>by: </label>
+            <input type="text" id="song-creator" />
+          </div>
+        </div>
+      </footer>
+
+      {/* Navigation buttons */}
+      <NavigationButtons />
+
+      {/* Dialogs */}
       <SaveDialog />
     </>
   );
